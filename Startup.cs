63ddf4520c79cf47c38b6d9a1f3b0b97eada39a4
Michael White <engiemcwhite@gmail.com>
2018-04-18 13:28:24 -0500

    changed logging

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,10 @@ namespace planner
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole();
                 app.UseDeveloperExceptionPage();
             }
             else
